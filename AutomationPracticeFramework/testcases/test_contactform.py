3c8738b9aa8443b1c8b01b0bdd5cd2241b0318cc
Mike Arias <michael.arias.93@gmail.com>
2022-03-06 11:03:53 -0400

    initial implementation of test_contact_us_form_components

---
+++ AutomationPracticeFramework/testcases/test_contactform.py
@@ -0,0 +1,30 @@
+import unittest
+from webdriver import Webdriver
+from values import strings
+
+
+class ContactUsFormTestSuite(unittest.TestCase):
+
+	def setUp(self):
+		self.driver = Driver()
+		self.driver.navigate(strings.base_url)
+
+	def test_contact_us_form_components(self):
+		self.homescreen = HomeScreen(self.driver)
+		self.homescreen.click_contact_us_button()
+
+		self.contact_us_form = ContactUsForm(self.driver)
+		self.contact_us_form.validate_form_title_is_present()
+		self.contact_us_form.validate_form_heading_is_present()
+		self.contact_us_form.validate_subjectf_field_is_present()
+		self.contact_us_form.validate_email_field_is_present()
+		self.contact_us_form.validate_order_reference_field_is_present()
+		self.contact_us_form.validate_attatch_file_field_is_present()
+		self.contact_us_form.validate_message_field_is_present()
+
+	def tearDown(self):
+        self.driver.instance.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
